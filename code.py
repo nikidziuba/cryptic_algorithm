cba0a29d2e813e1edaed235c8690786a7a820eca
nikidziuba <61624437+nikidziuba@users.noreply.github.com>
2021-07-01 15:39:03 +0200

    Simplified variables and adding a new function

--- code.py
+++ code.py
@@ -8,7 +8,7 @@ def encode(estr):
     #defining variables and a list for Random Choice
     alist = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'r', 's', 't', 'u', 'w', 'v', 'x', 'y', 'z', '0' ,'1', '2', '3', '4', '5' ,'6', '7', '8', '9']
     ei = 1
-    erint = ''
+    eri = ''
 
     #Taking 5 last numbers (not counting ones after the decimal separator) of current time as a key
     ekey = str(time.time())
@@ -20,11 +20,12 @@ def encode(estr):
     enc = ec.encoded
 
     #adding 10 chars randomly chosen from a list
-    while ei < 10:
-        erint = str(erint) + str(choice(alist))
+    while ei < (10 + int(ekey[:1])):
+        eri = str(eri) + str(choice(alist))
         ei += 1
+
     #combining them together
-    erdata = str(enc) + str(erint) 
+    erdata = str(enc) + str(eri) 
 
     #adding key to the string for decoding
     ekeydata = str(erdata) + str(ekey)
@@ -54,7 +55,8 @@ def decode(dstr):
     dkey = str(dbin)[-5:]
 
     #removing previously added 10 random chars and a key
-    drand = str(dbin)[:-14]
+    rm = (14 + int(dkey[:1])) * -1
+    drand = str(dbin)[:rm]
 
     #decoding with Caesar Cipher using extracted key as offset
     dec = CaesarCipher(drand, offset=int(dkey)).decoded
