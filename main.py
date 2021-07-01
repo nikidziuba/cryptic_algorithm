34943e5182cd653ddd09d5eed8229de3e9dc922f
nikidziuba <61624437+nikidziuba@users.noreply.github.com>
2021-07-01 14:44:22 +0200

    Updated to create a loop

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 from code import encode, decode
 
-print('Choose:')
-print('1. Encode')
-print('2. Decode')
-choice = input()
-if choice == '1':
-    str_to_encode = input('Enter a tring to encode: ')
-    encoded_str = encode(str_to_encode)
-    print('Encoded string: ' + encoded_str)
-elif choice == '2':
-    str_to_decode = input('Enter a tring to decode: ')
-    decoded_str = decode(str_to_decode)
-    print('Decoded string: ' + decoded_str)
-
-
+while True:
+  print('Choose:')
+  print('1. Encode')
+  print('2. Decode')
+  print('3. Quit')
+  choice = input()
+  if choice == '1':
+      str_to_encode = input('Enter a string to encode: ')
+      encoded_str = encode(str_to_encode)
+      print('Encoded string: ' + encoded_str)
+  elif choice == '2':
+      str_to_decode = input('Enter a string to decode: ')
+      decoded_str = decode(str_to_decode)
+      print('Decoded string: ' + decoded_str)
+  elif choice == '3':
+        break
